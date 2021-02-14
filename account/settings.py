ad4626404157ada72616389ccebedccebd38be4c
denilson <denilsonwash@gmail.com>
2021-02-14 12:59:33 +0300

    f

--- account/settings.py
+++ account/settings.py
@@ -29,9 +29,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -42,7 +44,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'account.urls'
-
+CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
